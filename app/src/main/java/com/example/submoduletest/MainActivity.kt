d5414de779a9719a128a689b0ba11be28bfe0985
Ian Lin <ian.lin@yjtech.tw>
2020-06-29 17:15:37 +0800

    [modify] subModuleTest

--- app/src/main/java/com/example/submoduletest/MainActivity.kt
+++ app/src/main/java/com/example/submoduletest/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.submoduletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun hideBottomNav() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showBottomNav() {
+        TODO("Not yet implemented")
     }
 }
